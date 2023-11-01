f98bf19e22eb716f9aa02fecf0376478f2fe68ca
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-11-01 13:16:24 +0700

    uuid

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -12,9 +12,9 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
-        children: const [
+        children: [
           Text('The Chart!'),
           Text('Expenses List'),
         ],
