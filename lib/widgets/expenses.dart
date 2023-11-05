99c5e99a24471ed5ffffc2c7389b0f4dbacf98d8
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-11-05 08:58:27 +0700

    remove expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -32,13 +32,19 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
       isScrollControlled: true,
       context: context,
-      builder: (ctx) => NewExpense(onAddExpense : _addExpense),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
 
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -58,7 +64,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart!'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           )
         ],
       ),
